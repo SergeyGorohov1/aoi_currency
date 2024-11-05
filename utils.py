cdc6c70bb5d4dbe8f193efa13c43f1233c2732db
Сергей <goroxov.99@mail.ru>
2024-11-05 13:56:06 +0300

    Add function get_stock_prices

--- utils.py
+++ utils.py
@@ -25,4 +25,27 @@ def get_currency_rates():
     return currency_rates
 
 
-print(get_currency_rates())
+def get_stock_prices():
+    with open("user_settings.json") as f:
+        data = json.load(f)
+
+    list_stocks = data["user_stocks"]
+
+    api_alpha_vanvage = os.getenv("API_KEY_ALPHA_VAVANGE")
+    interval = "1min"
+
+    list_stocks_prices = []
+    for stock in list_stocks:
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock}&interval={interval}&apikey={api_alpha_vanvage}'
+
+        data = requests.get(url).json()
+        last_refreshed = data["Meta Data"].get("3. Last Refreshed")
+        price_stock = data[f"Time Series ({interval})"].get(last_refreshed).get("1. open")
+        list_stocks_prices.append({"stock": stock, "price": price_stock})
+
+    return list_stocks_prices
+
+
+if __name__ == "__main__":
+    # print(get_currency_rates)
+    print(get_stock_prices())
